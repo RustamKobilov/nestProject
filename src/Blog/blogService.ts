8d7dbe0e5cda0c1076598c871ed90bf3175a4e6b
Rustam Kobilov <kobilov96@yandex.ru>
2023-06-28 21:25:22 +0300

    commit finish

--- src/Blog/blogService.ts
+++ src/Blog/blogService.ts
@@ -24,7 +24,7 @@ export class BlogService {
 
   async createNewBlog(createBlogDto: CreateBlogDTO): Promise<BlogViewModel> {
     //await this.blogRepository.checkDuplicateName(createBlogDto.name);
-    // data 6
+
     const blog: Blog = {
       id: randomUUID(),
       name: createBlogDto.name,

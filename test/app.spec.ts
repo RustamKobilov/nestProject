3f92f6227b92bb87e688d58bdfb799fccdadaf42
Rustam Kobilov <kobilov96@yandex.ru>
2023-07-05 18:21:01 +0300

    05/07/23 test

--- test/app.spec.ts
+++ test/app.spec.ts
@@ -1,21 +1,18 @@
 import { HttpStatus, INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { BlogController } from '../src/Blog/blogController';
-import { BlogService } from '../src/Blog/blogService';
-import { BlogRepository } from '../src/Blog/blogRepository';
 import { appSetting } from '../src/appSetting';
 import { CreateBlogDTO } from '../src/DTO';
 import request from 'supertest';
+import { AppModule } from '../src/app.module';
+
 describe('AppController', () => {
   let app: INestApplication;
   let httpServer;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      controllers: [BlogController],
-      providers: [BlogService, BlogRepository],
+      imports: [AppModule],
     }).compile();
-
     app = moduleFixture.createNestApplication();
     appSetting(app);
     await app.init();
